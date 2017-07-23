e65347b528838a755b795bc9f1e98f8aea7a5b75
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2017-07-23 22:08:05 +0100

    2.5.5 Exercise: Adding All The Things - pt2

--- src/main/scala/org/devrx/cats/superadder/SuperAdder.scala
+++ src/main/scala/org/devrx/cats/superadder/SuperAdder.scala
@@ -1,8 +1,7 @@
 package org.devrx.cats.superadder
 
 import cats.Monoid
-import cats.instances.int._
 
 object SuperAdder {
-  def add(items: List[Int]): Int = Monoid[Int].combineAll(items)
+  def add[A](items: List[A])(implicit m: Monoid[A]): A = m.combineAll(items)
 }
--- src/test/scala/org/devrx/cats/superadder/SuperAdderSpec.scala
+++ src/test/scala/org/devrx/cats/superadder/SuperAdderSpec.scala
@@ -1,16 +1,30 @@
 package org.devrx.cats.superadder
 
+import cats.instances.int._
+import cats.instances.option._
 import org.scalatest.{Matchers, WordSpec}
 
 class SuperAdderSpec extends WordSpec with Matchers {
 
   "add" should {
-    "return zero for empty list" in {
-      SuperAdder.add(List.empty) shouldBe 0
+    "sum ints" in {
+      SuperAdder.add(List(5, 10, 1)) shouldBe 16
     }
 
-    "return sum" in {
-      SuperAdder.add(List(5, 10, 1)) shouldBe 16
+    "return zero for empty list" in {
+      SuperAdder.add(List.empty[Int]) shouldBe 0
+    }
+
+    "sum Option[Int]s" in {
+      SuperAdder.add(List(None, Some(5), None, Some(10), Some(6))) shouldBe Some(21)
+    }
+
+    "return None for empty list" in {
+      SuperAdder.add(List.empty[Option[Int]]) shouldBe None
+    }
+
+    "return None when all Option[Int]s are None" in {
+      SuperAdder.add(List(None, None): List[Option[Int]]) shouldBe None
     }
   }
 }

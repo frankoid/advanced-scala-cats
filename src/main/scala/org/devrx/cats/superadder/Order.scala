dcbb8b1e644233dc5acc87cde8d58802d250a5cd
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2017-07-26 20:03:06 +0100

    2.5.5 Exercise: Adding All The Things - pt3 - monoids all the way down

--- src/main/scala/org/devrx/cats/superadder/Order.scala
+++ src/main/scala/org/devrx/cats/superadder/Order.scala
@@ -1,12 +1,15 @@
 package org.devrx.cats.superadder
 
 import cats.Monoid
+import cats.instances.double._
+import cats.syntax.semigroup._
 
 case class Order(totalCost: Double, quantity: Double)
 
 object Order {
   implicit val orderMonoid = new Monoid[Order] {
-    override def empty: Order = Order(0.0, 0.0)
-    override def combine(x: Order, y: Order): Order = Order(x.totalCost + y.totalCost, x.quantity + y.quantity)
+    override def empty: Order = Order(Monoid[Double].empty, Monoid[Double].empty)
+    override def combine(x: Order, y: Order): Order =
+      Order(x.totalCost |+| y.totalCost, x.quantity |+| y.quantity)
   }
 }

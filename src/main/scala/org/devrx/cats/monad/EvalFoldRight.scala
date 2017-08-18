6e37f6cc26a63738fe11a7dc534e7d71bc1b228c
Francis Devereux <francis@devrx.org>
2017-08-18 21:53:03 +0100

    4.5.5 Exercise: Safer Folding using Eval

---
+++ src/main/scala/org/devrx/cats/monad/EvalFoldRight.scala
@@ -0,0 +1,19 @@
+package org.devrx.cats.monad
+
+import cats.Eval
+
+object EvalFoldRight {
+
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B = {
+    def evalFoldRight(as: List[A], acc: Eval[B])(fn: (A, Eval[B]) => Eval[B]): Eval[B] =
+      as match {
+        case head :: tail =>
+          Eval.defer(fn(head, evalFoldRight(tail, acc)(fn)))
+        case Nil =>
+          acc
+      }
+
+    evalFoldRight(as, Eval.now(acc))((a, evalB) =>
+      evalB.flatMap((b: B) => Eval.defer(Eval.now(fn(a, b))))).value
+  }
+}

ef5ecea3296542b19aaa8c00e0b22172de788984
Francis Devereux <francis@devrx.org>
2017-09-13 23:03:21 +0100

    4.7.3 Exercise: Hacking on Readers

---
+++ src/main/scala/org/devrx/cats/monad/Db.scala
@@ -0,0 +1,35 @@
+package org.devrx.cats.monad
+
+import cats.Monad
+import cats.data.Reader
+
+case class Db(
+  usernames: Map[Int, String],
+  passwords: Map[String, String]
+)
+
+object Db {
+
+  type DbReader[A] = Reader[Db, A]
+
+  def findUsername(userId: Int): DbReader[Option[String]] =
+    Reader(db => db.usernames.get(userId))
+
+  def checkPassword(
+    username: String,
+    password: String
+  ): DbReader[Boolean] =
+    Reader(db => db.passwords.get(username).contains(password))
+
+  def checkLogin(
+    userId: Int,
+    password: String
+  ): DbReader[Boolean] =
+    for {
+      maybeUsername <- findUsername(userId)
+      passwordCorrect <- maybeUsername
+        .map(username => checkPassword(username, password))
+        .getOrElse(Monad[DbReader].pure(false))
+    } yield passwordCorrect
+
+}
